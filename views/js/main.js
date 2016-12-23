9de6bd0481e82a82140d29f7a9b719ca092b932a
aerosmith3430 <aerosmith3430@gmail.com>
2016-12-22 20:27:28 -0800

    Modified main.js

--- views/js/main.js
+++ views/js/main.js
@@ -490,9 +490,9 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.getElementsByClassName("mover"); // changed DOM selector
-  var scrollTop = document.body.scrollTop / 1250;
-  var phase = Math.sin(scrollTop + (i % 5));
+  var scrollTop = document.body.scrollTop / 1250;  
   for (var i = 0; i < items.length; i++) {
+    var phase = Math.sin(scrollTop + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
